bd39a8164d38b602a15b9cf0a90099a71366c1c8
Ben Obringer <obringer.20@osu.edu>
2016-02-22 22:44:38 -0500

    Readd 'server' specification

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'obringer20:meteor-discogs',
-  version: '1.1.1',
+  version: '1.1.2',
   summary: 'Retrieves music data from Discogs.',
   documentation: 'README.md',
   git: 'https://github.com/ben-obringer/meteor-discogs.git'
@@ -11,11 +11,11 @@ Npm.depends({ disconnect: '0.6.8' });
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
 
-  api.use('ecmascript');
+  api.use('ecmascript', 'server');
 
-  api.addFiles('meteor-discogs.js');
+  api.addFiles('meteor-discogs.js', 'server');
 
-  api.export('Discogs');
+  api.export('Discogs', 'server');
 });
 
 Package.onTest(function(api) {
@@ -24,7 +24,7 @@ Package.onTest(function(api) {
     'ecmascript',
     'sanjo:jasmine@0.20.3'
   ];
-  api.use(packages);
+  api.use(packages, 'server');
 
-  api.addFiles('meteor-discogs-tests.js');
+  api.addFiles('meteor-discogs-tests.js', 'server');
 });
